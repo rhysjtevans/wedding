fc1f930f6aae47e1cef13ec3b0b5f27de3823736
rhys@rhysevans.co.uk <rhys@rhysevans.co.uk>
2019-04-29 21:43:50 +0100

    sorted

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM jekyll/jekyll
-COPY wedding-website/ /srv/jekyll/_site
-CMD jekyll serve --skip-initial-build
--- build.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-docker container run --rm --name=jekyll -v "/Users/rhysevans/OneDrive - CoreFlux/Documents/Projects/Wedding":/app/src -it -p 4000:4000 jekyll/jekyll -- jekyll build --config /app/src/_config.yml build
